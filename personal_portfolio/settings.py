4191c139e5300903c8417cdedcb1b057ea07c3d1
maybe-my <loading4512@gmail.com>
2023-01-04 22:36:42 +0200

    edit settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z(o#z(vy**tn)o5%f*w8g^a228!m4%%jorn#9h-zgsjlwp#isg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['loadmast.pythonanywhere.com']
 
 
 # Application definition
