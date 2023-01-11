9bc17abbb5537edf6bdca9cf7a56453379d4e33f
loadlinux <loading4512@gmail.com>
2023-01-12 00:41:35 +0200

    fix templates bags

--- PortfolioApp/static/PortfolioApp/scripts/main.js
+++ PortfolioApp/static/PortfolioApp/scripts/main.js
@@ -1,4 +1,5 @@
 let header__container = document.querySelector('.header__container');
+let a1 = document.querySelector('.a1');
 let a2 = document.querySelector('.a2');
 let a3 = document.querySelector('.a3');
 let a4 = document.querySelector('.a4');
@@ -12,6 +13,10 @@ burger.addEventListener('click', function(){
 	header__container.classList.toggle('active');
 })
 
+a1.addEventListener('click', function(){
+	header__container.classList.remove('active');
+	burger.classList.remove('open--burger');
+})
 a2.addEventListener('click', function(){
 	header__container.classList.remove('active');
 	burger.classList.remove('open--burger');
